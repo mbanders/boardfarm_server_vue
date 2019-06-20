2d2f5fd1457c8a87a8e3fce930a41a06d21e4b37
Mike Anderson <mbanderson@uwalumni.com>
2019-06-19 19:26:58 -0500

    Clean up dashboard a bit
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="app">
     <AppHeader fixed>
-      <SidebarToggler class="d-lg-none" display="md" mobile />
+      <!-- <SidebarToggler class="d-lg-none" display="md" mobile /> -->
       <b-link class="navbar-brand" to="#">
         <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
         <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </b-link>
-      <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
+      <!-- <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true /> -->
       <b-navbar-nav class="d-md-down-none">
         <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
-        <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
-        <b-nav-item class="px-3">Settings</b-nav-item>
+        <!-- <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
+        <b-nav-item class="px-3">Settings</b-nav-item> -->
       </b-navbar-nav>
-      <b-navbar-nav class="ml-auto">
+      <!-- <b-navbar-nav class="ml-auto">
         <b-nav-item class="d-md-down-none">
           <i class="icon-bell"></i>
           <b-badge pill variant="danger">5</b-badge>
@@ -24,8 +24,8 @@
           <i class="icon-location-pin"></i>
         </b-nav-item>
         <DefaultHeaderDropdownAccnt/>
-      </b-navbar-nav>
-      <AsideToggler class="d-none d-lg-block" />
+      </b-navbar-nav> -->
+      <!-- <AsideToggler class="d-none d-lg-block" /> -->
       <!--<AsideToggler class="d-lg-none" mobile />-->
     </AppHeader>
     <div class="app-body">
