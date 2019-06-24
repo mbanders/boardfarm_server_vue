7498e054cd5338aa596bb2e775a27bd99e7daca3
Mike Anderson <mbanderson@uwalumni.com>
2019-06-23 21:39:53 -0500

    Improve about page
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="animated fadeIn">
-    <h3>About</h3>
-    <p>About boardfarm server.</p>
+    <b-col sm="6" lg="6">
+      <b-card class="shadow">
+        <h3>About</h3>
+        <p>Boardfarm server makes it easy to manage a large set of test stations. It presents a live view
+          of the status of equipment, and facilitates the "checking out" of test equipment.
+          The command-line <a href="https://github.com/lgirdk/boardfarm">boardfarm</a> test framework contacts
+          this server to find available stations for use.
+        </p>
+      </b-card>
+    </b-col>
   </div>
 </template>
 
